d133345005ddf143605d5b061c5fbf207d265aa4
Samantha Kreisel <samikreisel@gmail.com>
2022-11-06 10:27:48 -0600

    init

---
+++ src/main/java/com/launch/launchlearning/LaunchlearningApplication.java
@@ -0,0 +1,13 @@
+package com.launch.launchlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchlearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launch/launchlearning/LaunchlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launch.launchlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
